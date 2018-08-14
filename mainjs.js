6f77a9ece3c5b1bf3fbdc3a7cbeeb5b15e95523e
aruluki3 <arularul970@gmail.com>
2018-08-14 10:36:57 +0530

    Editing

--- mainjs.js
+++ mainjs.js
@@ -1,6 +1,7 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
+
 $(document).ready(function(){
   SC.stream('/tracks/340282793',function(sound){
     $('#start').click(function(e){
@@ -193,7 +194,7 @@ $(document).ready(function(){
   });
 
   SC.stream('/tracks/4743563',function(sound){
-    $('#start80').click(functio38212251n(e){
+    $('#start80').click(function(e){
       e.preventDefault();
       sound.start();
     });
